0f765a6f52cf3978f6a49018e514957e1b531fab
Burak Aktas <aburakaktas@gmail.com>
2018-09-03 09:24:36 +0300

    imported json module

--- run.py
+++ run.py
@@ -1,14 +1,19 @@
 import requests
 import re
+import sys
+import json
 
-saveFileFlag = False
+print(sys.version)
+
+saveFileFlag = 1
 header = {"User-Agent": "My User-Agent 1.0", "From": "youremail@domain.com"}
 r = requests.get(
     'https://www.sahibinden.com/ilan/vasita-otomobil-bmw-auto-reza-2012-bmw-640d-grand-coupe-distronic-head-up-masaj-600139580/detay', headers=header)
 content = r.text
 
+print(content)
 
-if saveFileFlag:
+if saveFileFlag == 1:
     try:
         file = open("source.txt", "w")
         file.write(content)
@@ -23,10 +28,12 @@ regex_sequence = re.compile(
 for match in regex_sequence.finditer(content):
     jsonStringList.append(match.group(1)),
 
-foundJsonCount = len(jsonStringList)
-if foundJsonCount > 0:
-    print(str(foundJsonCount) + " Json pattern(s) found.\n")
-    for item in jsonStringList:
-        print(item)
-else:
-    print("No Json pattern found.")
+# foundJsonCount = len(jsonStringList)
+# if foundJsonCount > 0:
+#     print(str(foundJsonCount) + " Json pattern(s) found.\n")
+#     for item in jsonStringList:
+#         print(item)
+# else:
+#     print("No Json pattern found.")
+
+# obj = json.loads(jsonStringList)
