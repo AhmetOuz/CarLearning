6a86d1e8163d0d5bf053d43fba9a047becddde95
Burak Aktas <aburakaktas@gmail.com>
2018-09-06 01:05:45 +0300

    create while loop, organize and merge some for loops, currently working with ozkan's method, to be fixed tomorrow

--- carListing.py
+++ carListing.py
@@ -6,30 +6,49 @@ import json
 print(sys.version)
 
 url = "https://www.sahibinden.com/audi-a3?pagingSize=50"
-
-saveFileFlag = 0
+saveFileFlag = True
+debugFlag = False
+hasNext = True
 header = {"User-Agent": "My User-Agent 1.0", "From": "youremail@domain.com"}
-r = requests.get(url, headers=header) 
-content = r.text
-
 linkList = []
-regex_sequence = re.compile(
-    r"<a\W+class\W*=\W*\"\W*classifiedTitle\W*\"\W+href\W*=\W*\"(.*)\"\W*>", re.MULTILINE)
-for match in regex_sequence.finditer(content):
-    linkList.append(match.group(1))
-    
-linkCount = len(linkList)
-if linkCount > 0:
-    # for item in linkList:
-        # obj = json.loads(item)
-        # print (obj)
-        # print ("=="*64)
-    	# print (item)    
-    print("\n" + str(linkCount) + " Link(s) found.\n")
-else:
-	print("No links found.")
+pageList = []
+findLinksSequence = re.compile(r"<a\W+class\W*=\W*\"\W*classifiedTitle\W*\"\W+href\W*=\W*\"(.*)\"\W*>", re.MULTILINE)
+nextPageSequence = re.compile(r"<div class=\"pageNavTable\">.*<a href=\"(.*)\" class=\"prevNextBut\">.*</a>.*</div>", re.MULTILINE|re.DOTALL)
+file = open("carlist.txt", "w")
 
-nextPage_sequence = re.compile(r"<div class=\"pageNavTable\">.*<a href=\"(.*)\" class=\"prevNextBut\">.*</a>.*</div>", re.MULTILINE|re.DOTALL)
-for match in nextPage_sequence.finditer(content):
-	print(match.group(0))
-	print("_$"*105)
+while hasNext == True:
+    hasNext = False
+    r = requests.get(url, headers=header) 
+    content = r.text
+
+    for match in findLinksSequence.finditer(content):
+        
+        linkList.append(match.group(1))
+        if saveFileFlag == 1:
+            file.write("https://www.sahibinden.com" + match.group(1) +"\n")
+
+    file.flush()
+            # print("File written successfully ;))")
+
+    if debugFlag == 1:
+        linkCount = len(linkList)
+        if linkCount > 0:
+            print("\n" + str(linkCount) + " Link(s) found.\n")
+        else:
+            print("No links found.")
+
+    #save file
+   
+
+    #next page
+    for match in nextPageSequence.finditer(content):
+        pageList.append(match.group(1))
+        print(pageList)
+        url = "https://www.sahibinden.com" + match.group(1)
+        hasNext = True
+
+
+
+    
+print (len(linkList))
+file.close()
